a96064d7cae223405582b89877391164dcac047f
Kirtana Mogili <kirtanamogili@gatech.edu>
2020-10-17 19:48:19 -0400

    Added methods

--- maps.py
+++ maps.py
@@ -3,25 +3,26 @@ import googlemaps
 from datetime import datetime
 
 sensitive = json.load("sensitive.json")
-gmaps = googlemaps.Client(key=sensitive.APIKey)
 
-origin = ""
-destination = ""
+def get_route(origin, destination):
+    gmaps = googlemaps.Client(key=sensitive.APIKey)
 
-# Request directions via walking
-now = datetime.now()
-directions_result = gmaps.directions(origin,
-                                     destination,
-                                     mode="walking",
-                                     departure_time=now)
+    # Request directions via walking
+    now = datetime.now()
+    directions_result = gmaps.directions(origin,
+                                        destination,
+                                        mode="walking",
+                                        departure_time=now)
+    return directions_result
 
-steps = directions_result["routes"][0]["legs"][0]["steps"][0]
 
 # Store image urls of route
-urls = []
+def get_route_img(directions_result):
+    urls = []
+    steps = directions_result["routes"][0]["legs"][0]["steps"][0]
 
-for step in steps:
-    end_location = step["end_location"]
-    lat = end_location["lat"]
-    lng = end_location["lng"]
-    urls.append("https://maps.googleapis.com/maps/api/streetview?location=" + lat + "," + lng + "&" + sensitive)
+    for step in steps:
+        end_location = step["end_location"]
+        lat = end_location["lat"]
+        lng = end_location["lng"]
+        urls.append("https://maps.googleapis.com/maps/api/streetview?location=" + lat + "," + lng + "&" + sensitive)
