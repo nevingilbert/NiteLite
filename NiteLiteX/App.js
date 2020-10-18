711e9e87299df67c234ee17f2938e89d088fe011
Kirtana Mogili <kirtanamogili@gatech.edu>
2020-10-18 00:49:52 -0400

    Front Page Design

--- NiteLiteX/App.js
+++ NiteLiteX/App.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import {
-  StyleSheet
+  StyleSheet,
+  View,
+  Text
 } from 'react-native';
 import Inputs from './Inputs.js'
 
 
 const App = () => {
   return (
-      <Inputs />
-  );
+		<View style={styles.main}>
+			<Text style={styles.title}>
+				NiteLite
+			</Text>
+    		<Inputs />
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
+	main: {
+		backgroundColor: "black",
+		flex: 1
+	},
+	title: {
+		color: '#7a42f4',
+		shadowColor: '#9a73ef',
+		fontSize: 50,
+		marginTop: 250,
+		marginLeft: 125,
+		marginRight: 120,
+		textAlignVertical: 'center'
+	}
 })
 
 export default App;
--- NiteLiteX/Inputs.js
+++ NiteLiteX/Inputs.js
@@ -19,23 +19,29 @@ class Inputs extends Component {
         return (
             <View style = {styles.container}>
                 <TextInput style = {styles.input}
+                    ref = {origin => { this.origin = origin }}
+                    color = "white"
                     underlineColorAndroid = "transparent"
                     placeholder = "Origin"
                     placeholderTextColor = "#9a73ef"
                     autoCapitalize = "none"
-                    onChangeText = {this.handleOrigin}/>
+                    onChangeText = {this.handleOrigin}
+                    clearButtonMode="always"/>
                 <TextInput style = {styles.input}
+                    ref = {destination => { this.destination = destination }}
+                    color = "white"
                     underlineColorAndroid = "transparent"
                     placeholder = "Destination"
                     placeholderTextColor = "#9a73ef"
                     autoCapitalize = "none"
-                    onChangeText = {this.handleDestination}/>
+                    onChangeText = {this.handleDestination}
+                    clearButtonMode="always"/>
                 <TouchableOpacity
-                    style = {styles.submitButton}
+                    style = {styles.enterButton}
                     onPress = {
-                        () => this.route(this.state.origin, this.state.destination)
+                        () => { this.route(this.state.origin, this.state.destination); this.origin.clear(); this.destination.clear(); }
                     }>
-                <Text style = {styles.submitButtonText}> Submit </Text>
+                <Text style = {styles.enterButtonText}> LET'S GO! </Text>
                 </TouchableOpacity>
             </View>
         )
@@ -44,23 +50,37 @@ class Inputs extends Component {
   export default Inputs
   
   const styles = StyleSheet.create({
-     container: {
-        paddingTop: 23
-     },
-     input: {
-        margin: 15,
+    container: {
+        paddingTop: 20,
+        marginTop: 15,
+        margin: 39
+    },
+    input: {
+        marginRight: 15,
+        marginLeft: 15,
+        marginTop: 15,
         height: 40,
         borderColor: '#7a42f4',
         borderWidth: 1,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
         padding: 10
-     },
-     submitButton: {
+    },
+    enterButton: {
         backgroundColor: '#7a42f4',
-        padding: 10,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        paddingTop: 10,
+        padding: 5,
         margin: 15,
         height: 40,
-     },
-     submitButtonText:{
+        width: 85
+    },
+    enterButtonText:{
         color: 'white'
-     }
-  })
+    }
+})
