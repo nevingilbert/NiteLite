f9706575e7aa4ce929107caaefa7b8ab37403608
Nevin Gilbert <nevin.gil@live.com>
2020-10-18 02:46:19 -0600

    learning rest api

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import azure_custom_vision
 import maps, yaml, safety
+from flask import Flask, json
+
+test = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+api = Flask(__name__)
+
+@api.route('/companies', methods=['GET'])
+def get_companies():
+  return json.dumps(test)
 
 if __name__ == "__main__":
-    origin = "clough undergraduate learning commons"
-    dest = "georgia tech campus recreation center"
+    api.run()
+    # origin = "clough undergraduate learning commons"
+    # dest = "georgia tech campus recreation center"
 
-    route_dict = safety.compute_data_all_routes(origin.replace(' ', '+'), dest.replace(' ', '+'))
-    for route in route_dict:
-        print('Route {} - Score {}'.format(route['id'], route['score_rel']))
+    # route_dict = safety.compute_data_all_routes(origin.replace(' ', '+'), dest.replace(' ', '+'))
+    # for route in route_dict:
+    #     print('Route {} - Score {}'.format(route['id'], route['score_rel']))
